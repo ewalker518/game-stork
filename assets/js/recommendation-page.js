819e4967072b995bb3067da56c86c2f1ca64f12c
Dylan Shoemaker <me@dylanshoemaker.com>
2021-12-08 20:19:57 -0800

    comment out google maps api

--- assets/js/recommendation-page.js
+++ assets/js/recommendation-page.js
@@ -1,44 +1,44 @@
 // Google Map
-let map;
-let service;
-let infoWindow
-function initMap() {
-  const saltLake = new google.maps.LatLng(40.7608, 111.8910);
-  infoWindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: saltLake,
-    zoom: 12,
-  });
-  const request = {
-    query: "best buy", 
-    fields: ["name", "geometry"],
-  };
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
+// let map;
+// let service;
+// let infoWindow
+// function initMap() {
+//   const saltLake = new google.maps.LatLng(40.7608, 111.8910);
+//   infoWindow = new google.maps.InfoWindow();
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: saltLake,
+//     zoom: 12,
+//   });
+//   const request = {
+//     query: "best buy", 
+//     fields: ["name", "geometry"],
+//   };
+//   service = new google.maps.places.PlacesService(map);
+//   service.findPlaceFromQuery(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+//       for (let i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
   
-}
+// }
 
 
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-    Animation: google.maps.Animation.BOUNCE,
+// function createMarker(place) {
+//   if (!place.geometry || !place.geometry.location) return;
+//   const marker = new google.maps.Marker({
+//     map,
+//     position: place.geometry.location,
+//     Animation: google.maps.Animation.BOUNCE,
    
-  });
-  google.maps.event.addListener(marker, "click", () => {
-    infoWindow.setContent(place.name || "");
-    infoWindow.open(map, marker,);
-  });
-}
+//   });
+//   google.maps.event.addListener(marker, "click", () => {
+//     infoWindow.setContent(place.name || "");
+//     infoWindow.open(map, marker,);
+//   });
+// }
 //Google Map
 
 function getGame() {
